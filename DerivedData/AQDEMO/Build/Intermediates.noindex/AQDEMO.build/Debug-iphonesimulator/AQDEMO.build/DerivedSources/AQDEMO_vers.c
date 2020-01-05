ec56ccd2d8a7cec0c71c22f90ffcdf97bb398c75
Safiqul <speak2safi@gmail.com>
2020-01-05 06:20:32 +0530

    demo sdk pod xcdc

---
+++ DerivedData/AQDEMO/Build/Intermediates.noindex/AQDEMO.build/Debug-iphonesimulator/AQDEMO.build/DerivedSources/AQDEMO_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char AQDEMOVersionString[];
+ extern const double AQDEMOVersionNumber;
+
+ const unsigned char AQDEMOVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:AQDEMO  PROJECT:AQDEMO-1" "\n";
+ const double AQDEMOVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/AQDEMO/Build/Products/Debug-iphonesimulator/AQDEMO.framework/Headers/AQDEMO.h
@@ -0,0 +1,19 @@
+//
+//  AQDEMO.h
+//  AQDEMO
+//
+//  Created by SAFIQUL ISLAM on 05/01/20.
+//  Copyright © 2020 AQDEMO-sdk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AQDEMO.
+FOUNDATION_EXPORT double AQDEMOVersionNumber;
+
+//! Project version string for AQDEMO.
+FOUNDATION_EXPORT const unsigned char AQDEMOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AQDEMO/PublicHeader.h>
+
+
